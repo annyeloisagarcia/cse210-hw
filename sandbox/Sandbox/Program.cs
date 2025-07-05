94cbeb9e01d91fd3931fa4fd451e4c7add124785
annyeloisagarcia <annygarciacabrera@gmail.com>
2025-07-05 00:13:25 -0400

    Changed the display message

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the sandbox Project anny. "); 
+        Console.WriteLine("Hello World! This is the sandbox Project. "); 
     }
 }
