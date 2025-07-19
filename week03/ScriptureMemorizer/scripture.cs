bbc1844b98019e246a17c6584e62f1892d7c9edb
annyeloisagarcia <annygarciacabrera@gmail.com>
2025-07-19 11:26:59 -0400

    Completed

---
+++ week03/ScriptureMemorizer/scripture.cs
@@ -0,0 +1,137 @@
+// The Scripture Memorizer is a console-based application that helps users memorize scripture passages by gradually hiding words from a verse or passage. 
+//It enhances memory through repetition and progressive recall.
+
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        
+        string[] wordArray = text.Split(' ');
+        foreach (string wordText in wordArray)
+        {
+            _words.Add(new Word(wordText));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random rnd = new Random();
+        int hiddenCount = 0;
+        int attempts = 0;
+
+        while (hiddenCount < numberToHide && attempts < _words.Count * 2)
+        {
+            int index = rnd.Next(_words.Count);
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+            attempts++;
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string display = _reference.GetDisplayText() + " ";
+        foreach (Word word in _words)
+        {
+            display += word.GetDisplayText() + " ";
+        }
+        return display.Trim();
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+                return false;
+        }
+        return true;
+    }
+}
+
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book =  book; 
+        _chapter = chapter; 
+        _verse = verse;
+        _endVerse = verse;
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = 3;
+        _verse = startVerse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_verse == _endVerse)
+        {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+    }
+}
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        {
+            return new string('_', _text.Length);
+        }
+        else
+        {
+            return _text;
+        }
+    }
+}
