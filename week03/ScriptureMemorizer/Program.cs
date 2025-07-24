1d634259324a2f01b92a2a14013685e075c6e40c
annyeloisagarcia <annygarciacabrera@gmail.com>
2025-07-23 20:35:50 -0400

    Completed

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,19 +1,38 @@
+// Creativity: Prompt user to choose a verse
 using System;
-using System.Collections.Generic;
- 
+
 class Program
-{  
+{
     static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Welcome to the Scripture Memorizer!");
+        Console.WriteLine("Press Enter to hide words or type 'quit' to exit.\n");
+
+        Reference reference = new Reference("1 Nephi", 3, 7);
+
+        Scripture scripture = new Scripture(reference,
+            "And it came to pass that I, Nephi, said unto my father: " +
+            "I will go and do the things which the Lord hath commanded, " +
+            "for I know that the Lord giveth no commandments unto the children of men, " +
+            "save he shall prepare a way for them that they may accomplish the thing which he commandeth them."
+        );
+
+        while (!scripture.IsCompletelyHidden())
         {
-                Reference ref1 = new Reference("1 Nephi", 3, 7);
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress Enter to hide more words or type 'quit' to end.");
 
-                Scripture scripture = new Scripture(ref1, "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+                break;
 
-                Console.WriteLine(scripture.GetDisplayText());
-
-                scripture.HideRandomWords(3);
-                Console.WriteLine(scripture.GetDisplayText());
-
-                Console.WriteLine("Is completely hidden? " + scripture.IsCompletelyHidden());
+            scripture.HideRandomWords(3);
         }
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+        Console.WriteLine("\nAll words are now hidden or you exited. Goodbye!");
+    }
 }
