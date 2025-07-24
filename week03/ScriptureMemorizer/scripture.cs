33ebf944112e666f30e8b01fbc6d487ab2944e0d
annyeloisagarcia <annygarciacabrera@gmail.com>
2025-07-23 20:37:08 -0400

    completed

--- week03/ScriptureMemorizer/scripture.cs
+++ week03/ScriptureMemorizer/scripture.cs
@@ -1,6 +1,3 @@
-// The Scripture Memorizer is a console-based application that helps users memorize scripture passages by gradually hiding words from a verse or passage. 
-//It enhances memory through repetition and progressive recall.
-
 using System;
 using System.Collections.Generic;
 
@@ -14,7 +11,6 @@ public class Scripture
         _reference = reference;
         _words = new List<Word>();
 
-        
         string[] wordArray = text.Split(' ');
         foreach (string wordText in wordArray)
         {
@@ -58,80 +54,5 @@ public class Scripture
                 return false;
         }
         return true;
-    }
-}
-
-public class Reference
-{
-    private string _book;
-    private int _chapter;
-    private int _verse;
-    private int _endVerse;
-
-    public Reference(string book, int chapter, int verse)
-    {
-        _book =  book; 
-        _chapter = chapter; 
-        _verse = verse;
-        _endVerse = verse;
-    }
-
-    public Reference(string book, int chapter, int startVerse, int endVerse)
-    {
-        _book = book;
-        _chapter = 3;
-        _verse = startVerse;
-        _endVerse = endVerse;
-    }
-
-    public string GetDisplayText()
-    {
-        if (_verse == _endVerse)
-        {
-            return $"{_book} {_chapter}:{_verse}";
-        }
-        else
-        {
-            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
-        }
-    }
-}
-
-public class Word
-{
-    private string _text;
-    private bool _isHidden;
-
-    public Word(string text)
-    {
-        _text = text;
-        _isHidden = false;
-    }
-
-    public void Hide()
-    {
-        _isHidden = true;
-    }
-
-    public void Show()
-    {
-        _isHidden = false;
-    }
-
-    public bool IsHidden()
-    {
-        return _isHidden;
-    }
-
-    public string GetDisplayText()
-    {
-        if (_isHidden)
-        {
-            return new string('_', _text.Length);
-        }
-        else
-        {
-            return _text;
-        }
     }
 }
